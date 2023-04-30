21c8349576cb8d266067c996bb87f78ea65e8918
david onwuli <dcazicruz@icloud.com>
2023-04-30 12:41:19 +0100

    task lockbox1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,16 +9,20 @@ def canUnlockAll(boxes):
     """if len(boxes) == 1:
         return True"""
     newboxes = []
+
     for i in range(len(boxes)):
         if len(boxes[i]) == 0:
             return False
               
         for j in range(len(boxes[i])):
-              if boxes[i][j] in newboxes or boxes[i][j] > len(boxes) - 1:
+            if boxes[i][j] in newboxes or boxes[i][j] > len(boxes) - 1:
                   break
-              else:
-                  newboxes.append(boxes[i][j])
-    if len(newboxes) == len(boxes):
+            elif boxes[i][j] == 0:
+                break
+            else:
+                newboxes.append(boxes[i][j])
+
+    if len(newboxes) == len(boxes) -1:
         return True
     else:
          return False
