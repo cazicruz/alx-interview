549ea87a42fc6595af5f88d21a070eb9459597e2
david onwuli <dcazicruz@icloud.com>
2023-05-18 21:37:53 +0100

    task0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" utf-8 validator script
+checks if the list of integers represent the correct encoding
+of the utf-8"""
+from typing import List
+
+
+def check(num):
+        mask = 1 << (8-1)
+        i = 0
+        while num & mask:
+            mask >>= 1
+            i += 1
+        return i
+
+
+def validUTF8(data: List) -> bool:
+    i = 0
+    while i < len(data):
+            j = check(data[i])
+            k = i + j - (j != 0)
+            i += 1
+            if j == 1 or j > 4 or k >= len(data):
+                return False
+            while i < len(data) and i <= k:
+                cur = check(data[i])
+                if cur != 1: return False
+                i += 1
+        return True
